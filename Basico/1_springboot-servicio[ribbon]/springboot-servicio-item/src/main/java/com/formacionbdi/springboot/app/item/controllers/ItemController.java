eacf64b9e7d2bf6b4b366ab5391108503b81d031
Erick <ericklj0509_@ciencias.unam.mx>
2026-04-03 19:45:48 -0600

    Se agrega la carpeta

---
+++ Basico/1_springboot-servicio[ribbon]/springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -0,0 +1,41 @@
+package com.formacionbdi.springboot.app.item.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionbdi.springboot.app.item.models.Item;
+import com.formacionbdi.springboot.app.item.models.service.ItemService;
+
+@RestController
+public class ItemController {
+	
+	@Autowired
+	//@Qualifier("serviceRestTemplate")
+	@Qualifier("serviceFeign")
+	private ItemService itemService;
+	
+	@GetMapping("/listar")
+	public List<Item> listar(){
+		return itemService.findAll();
+	}
+	
+	@GetMapping("/ver/{id}/cantidad/{cantidad}")
+	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return itemService.findById(id, cantidad);
+	}
+	
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id) {
+		itemService.delete(id);
+	}
+
+}
